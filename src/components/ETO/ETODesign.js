75d9c1e8e44754f12038761baf259a0a09987251
Claire Guo <claireguo@gmail.com>
2025-05-21 10:33:59 -0700

    large images fixed

--- src/components/ETO/ETODesign.js
+++ src/components/ETO/ETODesign.js
@@ -9,7 +9,7 @@ function ETODesign() {
     <Section tagText="Research" project="eto">
       <div className="bg-bg-gray border-[1px] border-border-default rounded-[16px] px-lg py-md lg:px-4xl lg:py-xl w-full flex justify-center">
         <img
-          src="images.claireguo.com/demo.gif"
+          src="https://images.claireguo.com/demo.gif"
           alt=""
           className="max-w-[260px] border-[1px] border-border-default rounded-[24px]"
         />
@@ -30,7 +30,7 @@ function ETODesign() {
           their natural stocks.
         </p>
         <img
-          src="images.claireguo.com/featurefish.gif"
+          src="https://images.claireguo.com/featurefish.gif "
           alt="Gif of ETO's Fish page. A list of fish images. Tapping on a fish takes you to a page with more information about the fish."
           className="max-w-[260px] border-[1px] border-border-default rounded-[24px] mx-sm"
         />
--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -9,17 +9,26 @@ function ProjectSummary() {
   return (
     <div className="flex gap-xs lg:text-sm lg:text-sm lg:justify-between lg:gap-[3rem] text-xs lg:text-sm">
       <div className="flex flex-col gap-3xs lg:gap-xs flex-1">
-        <span className="font-mono text-text-subtle text-sm"> Category </span>
+        <span className="font-mono text-text-subtle text-sm italic">
+          {" "}
+          Category{" "}
+        </span>
         <p> {project.category} </p>
       </div>
 
       <div className="flex flex-col gap-3xs lg:gap-xs flex-1">
-        <span className="font-mono text-text-subtle text-sm"> Team </span>
+        <span className="font-mono text-text-subtle text-sm italic">
+          {" "}
+          Team{" "}
+        </span>
         <p> {project.team} </p>
       </div>
 
       <div className="flex flex-col gap-3xs lg:gap-xs flex-1">
-        <span className="font-mono text-text-subtle text-sm"> Duration </span>
+        <span className="font-mono text-text-subtle text-sm italic">
+          {" "}
+          Duration{" "}
+        </span>
         <p> {project.duration} </p>
       </div>
     </div>
